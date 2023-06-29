55bb75e6762859bd072bfd89c23bde9737883756
Samarjiit <samarjitmahi6245@gmail.com>
2023-06-29 16:45:12 +0530

    ui setup and handling laoding,error and ready statee

---
+++ src/StartScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function StartScreen({ numQuestions }) {
+  return (
+    <div className="start">
+      <h2>welcome to the react quiz!</h2>
+      <h3>{numQuestions} questions to test your react mastery</h3>
+      <button className="btn btn-ui">Let's start</button>
+    </div>
+  );
+}
